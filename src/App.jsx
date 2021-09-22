f265f013be9683da850aeb209cd90c777e82b481
William <will2b@gmail.com>
2021-09-21 22:16:15 -0700

    edit marked previewer & css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import marked from "marked";
 import ReactHtmlParser from "react-html-parser";
 import "./App.css";
+import DEFAULT_MARKDOWN from "./default-markdown";
 
 marked.setOptions({
   breaks: true,
@@ -10,13 +11,22 @@ marked.setOptions({
 function App() {
   const [markdown, setMarkdown] = useState("");
 
+  useEffect(() => {
+    setMarkdown(DEFAULT_MARKDOWN);
+  }, []);
+
   const onChangeHandler = (event) => {
     setMarkdown(event.target.value);
   };
 
   return (
     <div className="App">
-      <textarea id="editor" value={markdown} onChange={onChangeHandler} />
+      <main>
+        <h1 className="heading">
+          React Markdown Previewer <span>by William Kang</span>
+        </h1>
+        <textarea id="editor" value={markdown} onChange={onChangeHandler} />
+      </main>
       <div id="preview">{ReactHtmlParser(marked(markdown))}</div>
     </div>
   );
